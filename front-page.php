6f78a04474ec5a0aa333d5872b115dc2288f9a4b
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2024-02-11 23:38:19 -0500

    juste des media sur front-page

--- front-page.php
+++ front-page.php
@@ -38,32 +38,33 @@ get_header();
       while ($accueil_posts_query->have_posts()) : $accueil_posts_query->the_post();
     ?>
         <article class="article-extrait bloc-flex-cl-ct">
-          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-          <?php
-          $contenu = get_the_content();
-          preg_match('/<svg.*?>(.*?)<\/svg>/s',  $contenu, $match_svg);
+          <a href="<?php the_permalink(); ?>">
+            <?php
+            $contenu = get_the_content();
+            preg_match('/<svg.*?>(.*?)<\/svg>/s',  $contenu, $match_svg);
 
-          if (!empty($match_svg[0])) {
-            // Affiche le contenu SVG
-            echo $match_svg[0];
-          } elseif (has_post_thumbnail()) {
-            // Affiche l'image mise en avant (post thumbnail) s'il n'y a pas de svg
-            $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'large');
-            echo '<img src="' . esc_url($thumbnail_url) . '" alt="' . esc_attr(get_the_title()) . '">';
-          } else {
-            preg_match('/<h2.*?>(.*?)<\/h2>/s',  $contenu, $match_h2);
-            if (!empty($match_h2[0])) {
-              // Affiche le contenu complet du H2 avec la balise H3
-              echo '<h3>' . $match_h2[1] . '</h3>';
+            if (!empty($match_svg[0])) {
+              // Affiche le contenu SVG
+              echo $match_svg[0];
+            } elseif (has_post_thumbnail()) {
+              // Affiche l'image mise en avant (post thumbnail) s'il n'y a pas de svg
+              $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'large');
+              echo '<img src="' . esc_url($thumbnail_url) . '" alt="' . esc_attr(get_the_title()) . '">';
             } else {
-              // Affiche le titre de l'article et les 20 premiers mots de the_content() avec la balise H3
-              echo '<h3><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></h3>';
-              $contenu_mots = preg_split("/[\s,]+/", strip_tags($contenu));
-              $extrait = implode(' ', array_slice($contenu_mots, 0, 20));
-              echo wpautop($extrait) . '...'; // Ajout de la balise <p> et des points de suspension
+              preg_match('/<h2.*?>(.*?)<\/h2>/s',  $contenu, $match_h2);
+              if (!empty($match_h2[0])) {
+                // Affiche le contenu complet du H2 avec la balise H3
+                echo '<h3>' . $match_h2[1] . '</h3>';
+              } else {
+                // Affiche le titre de l'article et les 20 premiers mots de the_content() avec la balise H3
+                echo '<h3><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></h3>';
+                $contenu_mots = preg_split("/[\s,]+/", strip_tags($contenu));
+                $extrait = implode(' ', array_slice($contenu_mots, 0, 20));
+                echo wpautop($extrait) . '...'; // Ajout de la balise <p> et des points de suspension
+              }
             }
-          }
-          ?>
+            ?>
+          </a>
         </article>
     <?php
       endwhile;
