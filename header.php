c6a987930ef7d8e75c043a102016111e428864ec
Noémie dS <112022872+NoemieDS@users.noreply.github.com>
2024-03-06 09:28:22 -0500

    barre admin WP présente, corr header

--- header.php
+++ header.php
@@ -22,8 +22,9 @@
 </head>
 
 <body <?php body_class(); ?>> <!-- ajoute des classes et des infos personnalisées en fonction de la page affichée  -->
+	<?php wp_body_open(); ?> <!-- permet d'afficher la barre admin de WP en tout temps -->
 
-	<header class="entete-barre bloc-flex-rw-ct">
+	<header class="entete-barre bloc-flex-cl-ct">
 		<div class="wrapper-entete-pied bloc-flex-rw-ct">
 			<?php
 			the_custom_logo();
